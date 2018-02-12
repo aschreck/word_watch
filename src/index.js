cdbfcf48c2643f624c1b4b34ab1b606134027637
Aaron Schreck <schreck.aaron@gmail.com>
2018-02-12 10:33:33 -0700

    words appear on side of page when you give paragraph

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  var word
-  var count
+  addApiWord()
+  handleParagraphs()
+})
 
+function addApiWord() {
   fetch("https://wordwatch-api.herokuapp.com/api/v1/top_word")
     .then((response) => {
       return response.json()
@@ -14,4 +16,36 @@ $(document).ready(() => {
       $(".top-word").text(`Top Word From Wordwatch API: ${word} (${number})`)
     })
     .catch(function (error) { console.error(error) })
-})
+}
+
+//add a listener on the text box.
+
+function handleParagraphs() {
+  $("button").on("click", function(){
+    var paragraph = $("textarea").val()
+   var splitPara = paragraph.split(' ')
+    var tally = splitPara.reduce(function(tally, word){
+      if (!tally[word]){
+        tally[word] = 1
+      } else {
+        tally[word] += 1
+      }
+      return tally
+    },{})
+    //paste it to the page with alternating sizing.
+    for (var word in tally)  {
+      //append it to the page with sizing
+      if (tally[word] == 1) {
+        $(".word-count").append(`<p style="font-size:10px;">${word}</p>`)
+      } else if (tally[word] == 2) {
+        $(".word-count").append(`<p style="font-size:20px;">${word}</p>`)
+      } else if (tally[word] == 3) {
+        $(".word-count").append(`<p style="font-size:30px;">${word}</p>`)
+      } else if (tally[word] == 4) {
+        $(".word-count").append(`<p style="font-size:40px;">${word}</p>`)
+      } else if (tally[word] == 3) {
+        $(".word-count").append(`<p style="font-size:50px;">${word}</p>`)
+      }
+    }
+  })
+}
