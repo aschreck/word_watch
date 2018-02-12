b5eb7c03f589362f7649bc1a02fb494e22a3c6f8
Aaron Schreck <schreck.aaron@gmail.com>
2018-02-12 09:20:25 -0700

    put basic functionality into actually the right file

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   // have fun!
+  var word
+
+  fetch("https://wordwatch-api.herokuapp.com/api/v1/top_word", {
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+    .then(function () {
+      debugger
+    })
+    .catch(function (error) { console.error(error) })
+  $('.top-word')
 })
