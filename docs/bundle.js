01e7d5a8c4100171d7b64eaa8e479d5307fa1639
Aaron Schreck <schreck.aaron@gmail.com>
2018-02-12 12:02:10 -0700

    change

--- docs/bundle.js
+++ docs/bundle.js
@@ -81,10 +81,100 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
 
 
-__WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).ready(() => {
-  // have fun!
+__WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).ready(function() {
+  addApiWord()
+  btnListener()
+  enterListener()
 })
 
+function addApiWord() {
+  fetch("https://wordwatch-api.herokuapp.com/api/v1/top_word")
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(response) {
+      var word = Object.keys(response.word)[0]
+      var number = response.word[word]
+      __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".top-word").text(`Top Word From Wordwatch API: ${word} (${number})`)
+    })
+    .catch(function (error) { console.error(error) })
+}
+
+function enterListener() {
+  __WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).keypress(function(e) {
+    if (e.which == '13') {
+      paragraphHandler()
+    }
+  })
+}
+
+function btnListener() {
+  __WEBPACK_IMPORTED_MODULE_0_jquery___default()("button").on("click", paragraphHandler)
+}
+
+function paragraphHandler() {
+  var processedParagraph = processInput()
+  var tally = tallyWordCount(processedParagraph)
+
+  for (var word in tally) {
+    postWordToDb(word)
+    appendWordToPage(tally, word)
+  }
+}
+
+function processInput() {
+  var paragraph = __WEBPACK_IMPORTED_MODULE_0_jquery___default()("textarea").val()
+  paragraph = paragraph.replace(/,/g, "")
+  paragraph = paragraph.replace(/\./g, ' ')
+  paragraph = paragraph.replace(/'/g, '');
+  paragraph = paragraph.replace(/-/g, '')
+
+  return paragraph.split(' ')
+}
+
+function tallyWordCount(words) {
+  var tally = words.reduce(function (tally, word) {
+    word = word.toLowerCase()
+    if (!tally[word]) {
+      tally[word] = 1
+    } else {
+      tally[word] += 1
+    }
+    return tally
+  }, {})
+  return tally
+}
+
+function postWordToDb (inputWord){
+  var jsonObj = {word: {value: inputWord}}
+  fetch("https://wordwatch-api.herokuapp.com/api/v1/words",{
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify(jsonObj)
+  })
+  .catch(function(error) {console.error("error:", error)})
+}
+
+function appendWordToPage(tally, word) {
+  if (tally[word] == 1) {
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".word-count").append(`<p style="font-size:10px;">${word}</p>`)
+  } else if (tally[word] == 2) {
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".word-count").append(`<p style="font-size:20px;">${word}</p>`)
+  } else if (tally[word] == 3) {
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".word-count").append(`<p style="font-size:30px;">${word}</p>`)
+  } else if (tally[word] == 4) {
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".word-count").append(`<p style="font-size:40px;">${word}</p>`)
+  } else if (tally[word] == 5) {
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".word-count").append(`<p style="font-size:50px;">${word}</p>`)
+  } else if (tally[word] == 6) {
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".word-count").append(`<p style="font-size:60px;">${word}</p>`)
+  } else if (tally[word] == 7) {
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".word-count").append(`<p style="font-size:70px;">${word}</p>`)
+  }
+}
 
 /***/ }),
 /* 2 */
